7257827915eb1004c895784a3ff8727148d82da4
Souleymane Bagyogo <bsouleymane78@gmail.com>
2014-09-20 16:42:20 -0400

    Ajout de la classe Transaction Ajout de la classe Category

---
+++ bumget/Category.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace bumget
+{
+	public class Category
+	{
+		//Champs
+		private int id;
+		private string name;
+		private string description;
+
+		//Méthodes
+		public int Id
+		{
+			get{
+				return id;
+			}
+			set{
+				id = value;
+			}
+		}
+
+		public float Name
+		{
+			get{
+				return name;
+			}
+			set{
+				name = value;
+			}
+		}
+
+		public float Description
+		{
+			get{
+				return description;
+			}
+			set{
+				description = value;
+			}
+		}
+
+		public Category ()
+		{
+
+		}
+
+	}
+}
+
